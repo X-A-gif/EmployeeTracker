c87d438a65502058934c224218a84c6d897cdb28
enamorada <109630476+AlmaHakba@users.noreply.github.com>
2023-03-23 13:00:21 -0700

    added connection and prompt

---
+++ index.js
@@ -0,0 +1,131 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to database as id');
+});
+
+// connection.query('SELECT * FROM employee', (err, rows) => {
+//   if (err) throw err;
+//   console.table(rows);
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit',
+      ],
+    },
+  ])
+  .then((answer) => {
+    switch (answer.action) {
+      case 'View all departments':
+        viewAllDepartments();
+        break;
+      case 'View all roles':
+        viewAllRoles();
+        break;
+      case 'View all employees':
+        viewAllEmployees();
+        break;
+      case 'Add a department':
+        addDepartment();
+        break;
+      case 'Add a role':
+        addRole();
+        break;
+      case 'Add an employee':
+        addEmployee();
+        break;
+      case 'Update an employee role':
+        updateEmployeeRole();
+        break;
+      case 'Exit':
+        connection.end();
+        break;
+    }
+  });
+
+  function viewAllDepartments() {
+    connection.query('SELECT * FROM departments', (err, res) => {
+      if (err) throw err;
+  
+      console.table(res);
+  
+      // promptUser();
+    });
+  }
+
+  function promptUser() {
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'action',
+          message: 'What would you like to do?',
+          choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Exit',
+          ],
+        },
+      ])
+      .then(function (answer) {
+        switch (answer.action) {
+          case 'View all departments':
+            viewAllDepartments();
+            break;
+          case 'View all roles':
+            viewAllRoles();
+            break;
+          case 'View all employees':
+            viewAllEmployees();
+            break;
+          case 'Add a department':
+            addDepartment();
+            break;
+          case 'Add a role':
+            addRole();
+            break;
+          case 'Add an employee':
+            addEmployee();
+            break;
+          case 'Update an employee role':
+            updateEmployeeRole();
+            break;
+          case 'Exit':
+            console.log('Goodbye!');
+            connection.end();
+            break;
+        }
+      });
+  }
+  
