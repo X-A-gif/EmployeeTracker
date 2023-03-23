a2af79c4d13a901aee9016de0fd66513d21af4ae
enamorada <109630476+AlmaHakba@users.noreply.github.com>
2023-03-23 13:00:58 -0700

    added connection and prompt

--- index.js
+++ index.js
@@ -17,11 +17,6 @@ connection.connect((err) => {
   console.log('Connected to database as id');
 });
 
-// connection.query('SELECT * FROM employee', (err, rows) => {
-//   if (err) throw err;
-//   console.table(rows);
-// });
-
 inquirer
   .prompt([
     {
@@ -75,57 +70,7 @@ inquirer
   
       console.table(res);
   
-      // promptUser();
+
     });
   }
 
-  function promptUser() {
-    inquirer
-      .prompt([
-        {
-          type: 'list',
-          name: 'action',
-          message: 'What would you like to do?',
-          choices: [
-            'View all departments',
-            'View all roles',
-            'View all employees',
-            'Add a department',
-            'Add a role',
-            'Add an employee',
-            'Update an employee role',
-            'Exit',
-          ],
-        },
-      ])
-      .then(function (answer) {
-        switch (answer.action) {
-          case 'View all departments':
-            viewAllDepartments();
-            break;
-          case 'View all roles':
-            viewAllRoles();
-            break;
-          case 'View all employees':
-            viewAllEmployees();
-            break;
-          case 'Add a department':
-            addDepartment();
-            break;
-          case 'Add a role':
-            addRole();
-            break;
-          case 'Add an employee':
-            addEmployee();
-            break;
-          case 'Update an employee role':
-            updateEmployeeRole();
-            break;
-          case 'Exit':
-            console.log('Goodbye!');
-            connection.end();
-            break;
-        }
-      });
-  }
-  
